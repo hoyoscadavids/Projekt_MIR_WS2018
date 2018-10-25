0b1b30dc553909226d895286bb83521265d226dd
Hencom <simon9629@gmail.com>
2018-10-25 18:39:06 +0200

    WaterImplementation

---
+++ Projekt_MIR_WS2018/Assets/Scripts/BoatControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10f, 0f));
+	}
+
+}
---
+++ Projekt_MIR_WS2018/Assets/Scripts/WaterEffectController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffectController : MonoBehaviour {
+    private Vector3 newTransform; 
+	// Update is called once per frame
+	void Update () {
+        newTransform = new Vector3(this.transform.position.x, this.transform.position.y, GameObject.Find("First_Marker").transform.position.z);
+        this.transform.position = newTransform;
+
+	}
+}

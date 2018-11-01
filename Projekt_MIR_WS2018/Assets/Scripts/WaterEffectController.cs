9689c6df0d64b204de147e03229f808236c3c45a
Hencom <simon9629@gmail.com>
2018-11-01 10:45:12 +0100

    Implementation Water - Vuforia

--- Projekt_MIR_WS2018/Assets/Scripts/WaterEffectController.cs
+++ Projekt_MIR_WS2018/Assets/Scripts/WaterEffectController.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterEffectController : MonoBehaviour {
-    private Vector3 newTransform; 
+    private Vector3 newTransform;
 	// Update is called once per frame
 	void Update () {
         newTransform = new Vector3(this.transform.position.x, this.transform.position.y, GameObject.Find("First_Marker").transform.position.z);
         this.transform.position = newTransform;
-
-	}
+    }
 }

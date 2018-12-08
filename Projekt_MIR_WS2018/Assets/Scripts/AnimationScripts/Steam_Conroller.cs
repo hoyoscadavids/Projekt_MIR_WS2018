a5d1c11d0548705499d33200a4b186eafac15809
Hencom <simon9629@gmail.com>
2018-12-08 14:43:54 +0100

    FIrstTrySteamParticles

---
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Steam_Conroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steam_Conroller : MonoBehaviour {
+    private Animator outerFrameAnimator;
+	// Use this for initialization
+	void Awake() {
+        outerFrameAnimator = GameObject.Find("Outer_Frame_Water").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(outerFrameAnimator.GetBool("isSun") && outerFrameAnimator.GetBool("isWater"))
+        {
+            GameObject.Find("Steam").GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            GameObject.Find("Steam").GetComponent<ParticleSystem>().Stop();
+        }
+	}
+}

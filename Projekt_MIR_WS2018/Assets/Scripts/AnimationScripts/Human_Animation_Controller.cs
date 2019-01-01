134ec4329ec0799e8433ce92fd4a859074466de0
Hencom <simon9629@gmail.com>
2019-01-01 16:48:13 +0100

    Sun + Trigger walking up/down animation

---
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Human_Animation_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human_Animation_Controller : MonoBehaviour {
+
+    
+    void walkUpAnimationEnded()
+    {
+        gameObject.GetComponent<Animator>().SetBool("walkUp", false);
+    }
+    void walkDownAnimationEnded()
+    {
+        gameObject.GetComponent<Animator>().SetBool("walkDown", false);
+    }
+}
---
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Sun_Animation_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun_Animation_Controller : MonoBehaviour {
+    private Animator outerFrameAnimator;
+    // Use this for initialization
+    void Awake()
+    {
+        outerFrameAnimator = GameObject.Find("Outer_Frame_Water").GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Island") || other.gameObject.CompareTag("Mountains"))
+        {
+            outerFrameAnimator.SetBool("isSun", true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Island") || other.gameObject.CompareTag("Mountains"))
+        {
+            outerFrameAnimator.SetBool("isSun", false);
+        }
+    }
+}

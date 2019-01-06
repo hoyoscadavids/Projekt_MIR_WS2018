7e2216c2979eb7fe6e56f577237b525b140cda5e
Hencom <simon9629@gmail.com>
2019-01-06 14:04:34 +0100

    Animation correction

--- Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Human_Animation_Controller.cs
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Human_Animation_Controller.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Human_Animation_Controller : MonoBehaviour {
+    private GameObject front, side;
 
-    
+    private void Awake()
+    {
+        front = GameObject.Find("Human_Front_Pivot");
+        side = GameObject.Find("Human_Side_Pivot");
+        side.SetActive(false);
+    }
     void walkUpAnimationEnded()
     {
         gameObject.GetComponent<Animator>().SetBool("walkUp", false);
@@ -12,5 +18,13 @@ public class Human_Animation_Controller : MonoBehaviour {
     void walkDownAnimationEnded()
     {
         gameObject.GetComponent<Animator>().SetBool("walkDown", false);
+    }
+    void changeToFront() {
+        front.SetActive(true);
+        side.SetActive(false);
+    }
+    void changeToSide() {
+        front.SetActive(false);
+        side.SetActive(true);
     }
 }
--- Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Mountain_Tilt_Animation_Controller.cs
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Mountain_Tilt_Animation_Controller.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class Mountain_Tilt_Animation_Controller : MonoBehaviour {
     Animator humanAnimator;
+    GameObject side;
     [SerializeField]
     float edgeRotation, maxRotation;
 	// Use this for initialization
 	void Awake() {
         humanAnimator = GameObject.Find("Human").GetComponent<Animator>();
+        side = GameObject.Find("side_body");
         edgeRotation = 0.2f;
         maxRotation = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.rotation.y <= -edgeRotation && this.transform.rotation.y > -maxRotation) {
-            humanAnimator.SetBool("walkUp", true);
-        }
-        else if (this.transform.rotation.y >= edgeRotation && this.transform.rotation.y < maxRotation)
+        if (this.transform.rotation.y <= -edgeRotation && this.transform.rotation.y > -maxRotation)
         {
+            humanAnimator.SetBool("walkUp", true);
+            humanAnimator.SetBool("walkDown", false);
+        }
+        /*  else if (this.transform.rotation.y >= edgeRotation && this.transform.rotation.y < maxRotation)
+          {
+              humanAnimator.SetBool("walkDown", true);
+              humanAnimator.SetBool("walkUp", false);
+          }*/
+        else
+        {
+            humanAnimator.SetBool("walkUp", false);
             humanAnimator.SetBool("walkDown", true);
         }
     }   

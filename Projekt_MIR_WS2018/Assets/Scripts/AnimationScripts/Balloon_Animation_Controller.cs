a2293c6392f2b18969a052fa2abe3604399c3bdb
Hencom <simon9629@gmail.com>
2019-01-06 17:40:50 +0100

    Anim walking/ BUgfixen

---
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Balloon_Animation_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon_Animation_Controller : MonoBehaviour {
+    private GameObject pfahl, head, side, front;
+    private Animator humanAnimator;
+    private bool headBool;
+	// Use this for initialization
+	void Awake () {
+        head = GameObject.Find("Kopf_Complete");
+        pfahl = GameObject.Find("Pfahl");
+        side = GameObject.Find("Human_Side_Pivot");
+        front = GameObject.Find("Human_Front_Pivot");
+        humanAnimator = GameObject.Find("Human").GetComponent<Animator>();
+        head.SetActive(false);
+        headBool = false;
+	}
+
+    private void Update()
+    {
+       // headBool = humanAnimator.GetBool("isRidingBalloon");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pfahl") && !humanAnimator.GetBool("isRidingBalloon"))
+        {
+            humanAnimator.SetBool("walkToBalloon", true);
+            humanAnimator.SetBool("walkBackFromBalloon", false);
+        }
+        else if (collision.gameObject.CompareTag("Pfahl") && humanAnimator.GetBool("isRidingBalloon")) {
+            head.SetActive(false);
+            humanAnimator.GetComponent<Animator>().SetBool("isRidingBalloon", false);
+            humanAnimator.SetBool("walkBackFromBalloon", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pfahl") && !humanAnimator.GetBool("isRidingBalloon")) {
+            humanAnimator.SetBool("walkToBalloon", false);
+            humanAnimator.SetBool("walkBackFromBalloon", true);
+        }
+    }
+}
--- Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Human_Animation_Controller.cs
+++ Projekt_MIR_WS2018/Assets/Scripts/AnimationScripts/Human_Animation_Controller.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Human_Animation_Controller : MonoBehaviour {
-    private GameObject front, side;
+    private GameObject front, side, head;
 
     private void Awake()
     {
         front = GameObject.Find("Human_Front_Pivot");
         side = GameObject.Find("Human_Side_Pivot");
+        head = GameObject.Find("Kopf_Complete");
         side.SetActive(false);
     }
     void walkUpAnimationEnded()
@@ -26,5 +27,22 @@ public class Human_Animation_Controller : MonoBehaviour {
     void changeToSide() {
         front.SetActive(false);
         side.SetActive(true);
+    }
+
+    void walkToBalloonAnimationEnded() {
+        gameObject.GetComponent<Animator>().SetBool("walkToBalloon", false);
+    }
+    void walkBackFromBalloonAnimationEnded()
+    {
+        Debug.Log("hi");
+        gameObject.GetComponent<Animator>().SetBool("walkBackFromBalloon", false);
+    }
+
+    void rideBalloon() {
+        front.SetActive(false);
+        side.SetActive(false);
+        head.SetActive(true);
+        gameObject.GetComponent<Animator>().SetBool("walkBackFromBalloon", false);
+        gameObject.GetComponent<Animator>().SetBool("isRidingBalloon", true);
     }
 }
